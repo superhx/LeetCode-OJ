a729d2c30e7acf20635e2d01c89beeeeb93d682b
SuperHx <hanxvdovehx@gmail.com>
2014-06-30 14:09:09 +0800

    ~

---
+++ Evaluate Reverse Polish Notation/Solution.java
@@ -0,0 +1,41 @@
+/*
+*Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+*
+*Valid operators are +, -, *, /. Each operand may be an integer or another expression.
+*
+*Some examples:
+*  ["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
+*  ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
+*/
+
+public class Solution {
+    public int evalRPN(String[] tokens) {
+        int prev;
+		int next;
+		Stack<Integer> stack = new Stack<Integer>();
+		for (int i = 0; i < tokens.length; i++) {
+			try {
+				stack.push(Integer.parseInt(tokens[i]));
+			} catch (Exception ex) {
+				next = stack.pop();
+				prev = stack.pop();
+				switch (tokens[i]) {
+				case "+":
+					prev = prev + next;
+					break;
+				case "-":
+					prev = prev - next;
+					break;
+				case "*":
+					prev = prev * next;
+					break;
+				case "/":
+					prev = prev / next;
+					break;
+				}
+				stack.push(prev);
+			}
+		}
+		return stack.pop();
+    }
+}
