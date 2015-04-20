94f63b444cf315bbe8652b13a8d5eb66b5de63fd
superhx <hanxvdovehx@gmail.com>
2015-04-20 19:35:14 +0800

    ~

---
+++ PathSum.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+public class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root==null) return false;
+        if(root.right==null&&root.left==null) return root.val==sum;
+        return hasPathSum(root.left,sum-root.val)||hasPathSum(root.right,sum-root.val);
+    }
+}
---
+++ PathSumII.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+public class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        Stack<Integer> path=new Stack<>();
+        List<List<Integer>> paths=new LinkedList<>();
+        pathSum(root,sum,path,paths);
+        return paths;
+    }
+
+    private void pathSum(TreeNode root,int sum,Stack<Integer> path,List<List<Integer>> paths){
+        if(root==null) return;
+        path.add(root.val);
+        if(root.left==null&&root.right==null){
+            if(root.val==sum) paths.add(new LinkedList<>(path));
+        }else{
+            pathSum(root.left,sum-root.val,path,paths);
+            pathSum(root.right,sum-root.val, path, paths);
+        }
+        path.pop();
+    }
+}
