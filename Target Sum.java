0ecd42f987231dedf3e54779f906c56af114e67b
Robin Han <hanxvdovehx@gmail.com>
2017-01-25 12:06:36 +0800

    ~

--- Target Sum.java
+++ Target Sum.java
@@ -1,19 +1,18 @@
 public class Solution {
-    // convert problem to subset sum problem
+    // Convert problem to subset sum problem. And the target(sum + S) must be even.
     public int findTargetSumWays(int[] nums, int S) {
         int target = S;
         for(int num: nums) target += num;
-        if (target / 2 < S) return 0;
-        for(int i = 0; i < nums.length; i++) nums[i] *= 2;
+        if (target / 2 < S || target % 2 > 0) return 0;
+        target = target / 2;
 
-        int[][] dp = new int[target + 1][nums.length + 1];
-        dp[0][0] = 1;
-        for(int i = 0; i < target + 1; i++) {
-            for(int j = 1; j < nums.length + 1; j++) {
-                dp[i][j] = dp[i][j - 1];
-                if (i - nums[j - 1] >= 0) dp[i][j] += dp[i - nums[j - 1]][j - 1];
+        int [] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int num: nums) {
+            for (int i = target; i >= num; i--) {
+                dp[i] += dp[i - num];
             }
         }
-        return dp[dp.length - 1][nums.length];
+        return dp[target];
     }
 }
